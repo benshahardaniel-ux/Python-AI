bc07b6d0e3129938977c9a68a0ad116c09bc899b
benshahardaniel-ux <benshahardaniel@gmail.com>
2026-01-07 17:04:00 +0200

    הוספתי הלפר והתחלתי את העמוד

--- helper.py
+++ helper.py
@@ -1,10 +1,22 @@
 from dotenv import load_dotenv
 import os
 import streamlit as st
+from google import genai
+
+all_models = [
+    "gemini-2.5-flash-lite",
+    "gemini-2.5-flash",
+    "gemini-2.0-flash-lite",
+    "gemini-2.0-flash",
+    "gemini-3.0-flash",]
+
+def create_chat():
+    client = genai.Client(api_key=getAPIKey())
+
 
 def getAPIKey():
     load_dotenv()
-    API_KEY = os.getenv('API_KEY')
+    API_KEY = os.getenv('API_KEY') or st.secrets['API_KEY']
     return API_KEY
 
 class Message:
